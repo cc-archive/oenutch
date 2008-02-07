94288bcec56e82656cde5d190002072949aa762e
nyergler <nyergler@committer.creativecommons.org>
2008-02-07 06:09:58 +0000

    Support transforming a license URI to license image URL.

--- nutch/src/java/org/creativecommons/learn/ResultHelper.java
+++ nutch/src/java/org/creativecommons/learn/ResultHelper.java
@@ -10,9 +10,26 @@ public class ResultHelper {
 	if (license_url.equals("http://creativecommons.org/licenses/by/3.0/"))
 	    return "green";
 
-	return "red";
+	// unknown
+	return "gray";
 	
     } // getLicenseCSS
+
+    public static String getLicenseImage (String license_url) {
+	// return the URL to the license image for the specied license
+
+	if (license_url.indexOf("http://creativecommons.org/licenses") != 0) 
+	    // not a CC license
+	    return null;
+
+	String result = license_url.replace(
+				     "http://creativecommons.org/licenses/",
+				     "http://i.creativecommons.org/l/");
+	result += "80x15.png";
+
+	return result;
+
+    } // getLicenseImage
 
     public static String getTagQueryHref (HttpServletRequest request,
 					  String tag) {
