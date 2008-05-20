6359b58278d515fc1643aa8f0d404c35547e7cb8
nyergler <nyergler@committer.creativecommons.org>
2008-05-20 20:16:05 +0000

    Finaly spelling correction.

--- nutch/src/java/org/creativecommons/learn/Search.java
+++ nutch/src/java/org/creativecommons/learn/Search.java
@@ -5,7 +5,7 @@ public class Search {
     public static String TAGS_FIELD = "tag";
     public static float  TAGS_BOOST = 2.0f;
 
-    public static String CURATOR_FIELD = "currator";
+    public static String CURATOR_FIELD = "curator";
     public static float  CURATOR_BOOST = 1.0f;
 
 } // Search
