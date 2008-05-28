1f70cf7a8060c2b05a82868c252013c33e3c7d60
nyergler <nyergler@committer.creativecommons.org>
2008-05-28 18:35:28 +0000

    Use "real" URL encoding and decoding; refactored link generation into slightly more generic approach.

--- nutch/src/java/org/creativecommons/learn/ResultHelper.java
+++ nutch/src/java/org/creativecommons/learn/ResultHelper.java
@@ -1,5 +1,8 @@
 package org.creativecommons.learn;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
 import java.util.Vector;
 
 import javax.servlet.http.HttpServletRequest;
@@ -9,77 +12,99 @@ import org.creativecommons.learn.oercloud.Curator;
 
 public class ResultHelper {
 
-	public static String getLicenseImage(String license_url) {
-		// return the URL to the license image for the specified license
-try{
-		if ((license_url == null) || (license_url.indexOf("http://creativecommons.org/licenses") != 0))
-			// not a CC license
-			return null;
+	private static String _getFullUrl(HttpServletRequest request) {
+		
+		StringBuffer url = request.getRequestURL();
+		url.append(request.getQueryString());
+		
+		return url.toString();
+	}
 
-		String result = license_url.replace(
-				"http://creativecommons.org/licenses/",
-				"http://i.creativecommons.org/l/");
-		result += "80x15.png";
-
-		return result;
-		} catch (NullPointerException e) {
-		   return null;
-		   }
-
-	} // getLicenseImage
-
-	public static String getLicenseQueryLink(HttpServletRequest request, String license_uri) {
-
+	private static String addQueryParameter(HttpServletRequest request, 
+			String query_key, String query_value) {
+		
 		// starting with a request, add a tag filter to the search
 		// and return the resulting HREF
-		String query = request.getParameter("query");
+		try {
+			String query = URLDecoder.decode(request.getParameter("query"), "UTF-8").trim();
+			query = query + " " + query_key + ":" + query_value + "";
+			query = URLEncoder.encode(query, "UTF-8");
+			
+			return request.getRequestURL().append("?query=" + query).toString();
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	
+		// fall back to just returning the existing URI
+		return _getFullUrl(request);
+		
+	} // addQueryParameter
+	
+	public static String getLicenseQueryLink(HttpServletRequest request,
+			String license_uri) {
 
-		query = query + "+" + Search.LICENSE_FIELD + ":\"" + license_uri + "\"";
-		return request.getRequestURL().toString() + "?query=" + query;
+		return addQueryParameter(request, Search.LICENSE_FIELD, license_uri);
 
 	} // getLicenseQueryLink
 
 	public static String getTagQueryHref(HttpServletRequest request, String tag) {
 
-		// starting with a request, add a tag filter to the search
-		// and return the resulting HREF
-		String query = request.getParameter("query");
-
-		query = query + "+tag:\"" + tag + "\"";
-		return request.getRequestURL().toString() + "?query=" + query;
+		return addQueryParameter(request, Search.TAGS_FIELD, tag);
 
 	} // getTagQueryHref
 
-	public static String[] getCuratorLinks(HttpServletRequest request, HitDetails result) {
+	public static String getCuratorQueryHref(HttpServletRequest request,
+			String curator) {
+
+		return addQueryParameter(request, Search.CURATOR_FIELD, curator);
+
+	} // getCuratorQueryHref
+
+	public static String[] getCuratorLinks(HttpServletRequest request,
+			HitDetails result) {
 
 		// get the curator(s) and resolve them to human readable name(s)
 		String[] curators = result.getValues(Search.CURATOR_FIELD);
-		if (curators == null) return new String[0];
+		if (curators == null)
+			return new String[0];
 
-                Vector<String> curator_links = new Vector<String>();
-		
+		Vector<String> curator_links = new Vector<String>();
+
 		for (int i = 0; i < curators.length; i++) {
-		System.out.println(curators[i]);
+			System.out.println(curators[i]);
 			Curator c = Curator.byUrl(curators[i]);
 
-			curator_links.add( "<a href=\"" + getCuratorQueryHref(request, curators[i]) + "\">" + c.getName() + "</a> <a href=\"" + curators[i] + "\"><img src=\"./img/house.png\" border=\"0\" /></a>" );
+			curator_links.add("<a href=\""
+					+ getCuratorQueryHref(request, curators[i]) + "\">"
+					+ c.getName() + "</a> <a href=\"" + curators[i]
+					+ "\"><img src=\"./img/house.png\" border=\"0\" /></a>");
 		}
-	
-		return (String[]) curator_links.toArray(new String[curator_links.size()]);
-		
+
+		return (String[]) curator_links
+				.toArray(new String[curator_links.size()]);
+
 	}
-	
-	public static String getCuratorQueryHref(HttpServletRequest request, String curator) {
 
-		// starting with a request, add a tag filter to the search
-		// and return the resulting HREF
-		String query = request.getParameter("query");
+	public static String getLicenseImage(String license_url) {
+		// return the URL to the license image for the specified license
+		try {
+			if ((license_url == null)
+					|| (license_url
+							.indexOf("http://creativecommons.org/licenses") != 0))
+				// not a CC license
+				return null;
 
-		query = query + "+" + Search.CURATOR_FIELD + ":'" + curator + "'";
-		System.out.println(Search.CURATOR_FIELD);
-		System.out.println(curator);
-		return request.getRequestURL().toString() + "?query=" + query;
+			String result = license_url.replace(
+					"http://creativecommons.org/licenses/",
+					"http://i.creativecommons.org/l/");
+			result += "80x15.png";
 
-	} // getSourceQueryHref
+			return result;
+		} catch (NullPointerException e) {
+			return null;
+		}
+
+	} // getLicenseImage
 
 } // ResultHelper
