3c51a5eccfa694c552b602d6941a3c856f5e3469
nyergler <nyergler@committer.creativecommons.org>
2008-06-06 17:22:41 +0000

    Initial support for periodic updates.

--- nutch/src/java/org/creativecommons/learn/aggregate/Main.java
+++ nutch/src/java/org/creativecommons/learn/aggregate/Main.java
@@ -6,7 +6,9 @@
 package org.creativecommons.learn.aggregate;
 
 import java.io.IOException;
+import java.util.Calendar;
 import java.util.Collection;
+import java.util.Date;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -25,23 +27,33 @@ public class Main {
      */
     public static void main(String[] args) {
 
+    	Calendar calendar = Calendar.getInstance();
+    	// set the update interval
+    	// (one day)
+    	calendar.add(Calendar.DATE, -1);
+    	
     	// get a list of all available feeds
     	Collection<Feed> all_feeds = TripleStore.get().load(Feed.class);
     	
         // process each one
         for (Feed feed : all_feeds) {
 
-            // XXX see if this feed needs to be re-imported
-            //if (feed.getLastImportDate().before(new Date())) {
+        	System.out.println(feed.getUrl());
+        	
+            // see if this feed needs to be re-imported
+            if (true) { // feed.getLastImport().before( calendar.getTime() )) {
                 try {
                     // re-import necessary
+                	System.out.println("updating...");
+
                 	new FeedUpdater(feed).update();
                 } catch (IOException ex) {
                     Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
                 } finally {
-                    // feed.setLastImportDate(new Date());                	
+                    feed.setLastImport(new Date());
+                    TripleStore.get().save(feed);
                 }
-            //}
+            }
             
             System.out.println(feed.getUrl());
 
